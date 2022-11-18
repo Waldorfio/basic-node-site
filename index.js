f944bbda33cfa57baecdb666d88f5413051b2150
Walid VM <walid.hanifi707@gmail.com>
2022-11-18 19:48:10 +1000

    Add url module for compatibility with all links

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const http = require('http');
+const url = require('url');
 const fs = require('fs');
-
+ 
 http.createServer(function (req, res) {
-    fs.readFile('index.html', function(err, data) {
+    const fullServerUrl = url.parse(req.url, true);
+    const filename = "."+ fullServerUrl.pathname;
+    fs.readFile(filename, function(err, data) {
+        if(err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }  
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data)
+        res.write(data);
         return res.end();
-    })
-}).listen(8080);
+    });
+}).listen(8080); 
